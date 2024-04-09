b576b81b20027a9dbfcd93d7c90901ee8f5a60d1
Miguel Pineda <miguel.pineda@imedicalservices.com>
2024-04-09 17:46:57 -0500

    add spa

--- packages/backend/lib/spa/index.ts
+++ packages/backend/lib/spa/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
+
+const build = path.resolve("build");
+const index = path.resolve("build/index.html");
 
 export default function reactAppMiddleware() {
   const router = express.Router();
 
-  router.use(express.static("build"));
+  router.use(express.static(build));
+  router.get("*", (req, res) => res.sendFile(index));
 
   return router;
 }
